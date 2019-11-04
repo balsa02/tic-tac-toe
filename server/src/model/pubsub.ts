d5810504297605e71260f78320c156e933693919
balsa02 <balsa02@gmail.com>
2019-11-04 23:07:20 +0100

    upgrade to async

--- server/src/model/pubsub.ts
+++ server/src/model/pubsub.ts
@@ -4,7 +4,7 @@ export class PubSub extends apollo.PubSub {
     constructor(options: apollo.PubSubOptions = {}) {
         super(options);
       }
-    public publish_with_result(triggerName: string, payload: any): Promise<boolean> {
-        return Promise.resolve(this.ee.emit(triggerName, payload));
+    public async publish_with_result(triggerName: string, payload: any): Promise<boolean> {
+        return this.ee.emit(triggerName, payload);
     }
 }
